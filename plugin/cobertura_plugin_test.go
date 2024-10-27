a230b2d18450068a1fc8e66710378b2145dc1e8e
Senthil Kumar T <c_senthil.kumar@harness.io>
2024-10-27 21:58:40 +0530

    added cobertura tests

--- plugin/cobertura_plugin_test.go
+++ plugin/cobertura_plugin_test.go
@@ -28,6 +28,95 @@ func TestCoberturaGoodThreshold(t *testing.T) {
 	_ = plugin
 }
 
+func TestCoberturaBadThreshold(t *testing.T) {
+
+	envPluginInputArgs := pd.EnvPluginInputArgs{
+		MinimumBranchCoverage:        0,
+		MinimumClassCoverage:         40,
+		MinimumLineCoverage:          5.0,
+		MinimumMethodCoverage:        0,
+		MinimumPackageCoverage:       100,
+		MinimumFileCoverage:          50.0,
+		MinimumLOC:                   15,
+		MinimumComplexityCoverage:    10,
+		MaxComplexityDensityCoverage: 0.50,
+	}
+
+	args := GetTestCoberturaNewArgs(envPluginInputArgs)
+	plugin, err := Exec(context.TODO(), args)
+	if err == nil {
+		t.Errorf("Error in TestCoberturaGoodThreshold: %s", err.Error())
+	}
+	_ = plugin
+}
+
+func TestCoberturaNoFailOnBadThreshold(t *testing.T) {
+
+	envPluginInputArgs := pd.EnvPluginInputArgs{
+		MinimumBranchCoverage:        0,
+		MinimumClassCoverage:         40,
+		MinimumLineCoverage:          5.0,
+		MinimumMethodCoverage:        0,
+		MinimumPackageCoverage:       100,
+		MinimumFileCoverage:          50.0,
+		MinimumLOC:                   15,
+		MinimumComplexityCoverage:    10,
+		MaxComplexityDensityCoverage: 0.50,
+	}
+
+	args := GetTestCoberturaNewArgs(envPluginInputArgs)
+	args.PluginFailOnThreshold = false
+	plugin, err := Exec(context.TODO(), args)
+	if err != nil {
+		t.Errorf("Error in TestCoberturaGoodThreshold: %s", err.Error())
+	}
+	_ = plugin
+}
+
+func TestCoberturaBadComplexityCoverage(t *testing.T) {
+
+	envPluginInputArgs := pd.EnvPluginInputArgs{
+		MinimumBranchCoverage:        0,
+		MinimumClassCoverage:         30,
+		MinimumLineCoverage:          5.0,
+		MinimumMethodCoverage:        0,
+		MinimumPackageCoverage:       100,
+		MinimumFileCoverage:          50.0,
+		MinimumLOC:                   15,
+		MinimumComplexityCoverage:    5,
+		MaxComplexityDensityCoverage: 0.50,
+	}
+
+	args := GetTestCoberturaNewArgs(envPluginInputArgs)
+	plugin, err := Exec(context.TODO(), args)
+	if err == nil {
+		t.Errorf("Error in TestCoberturaGoodThreshold: %s", err.Error())
+	}
+	_ = plugin
+}
+
+func TestCoberturaBadComplexityDensityCoverage(t *testing.T) {
+
+	envPluginInputArgs := pd.EnvPluginInputArgs{
+		MinimumBranchCoverage:        0,
+		MinimumClassCoverage:         30,
+		MinimumLineCoverage:          5.0,
+		MinimumMethodCoverage:        0,
+		MinimumPackageCoverage:       100,
+		MinimumFileCoverage:          50.0,
+		MinimumLOC:                   15,
+		MinimumComplexityCoverage:    10,
+		MaxComplexityDensityCoverage: 0.25,
+	}
+
+	args := GetTestCoberturaNewArgs(envPluginInputArgs)
+	plugin, err := Exec(context.TODO(), args)
+	if err == nil {
+		t.Errorf("Error in TestCoberturaGoodThreshold: %s", err.Error())
+	}
+	_ = plugin
+}
+
 func GetTestCoberturaNewArgs(envPluginInputArgs pd.EnvPluginInputArgs) pd.Args {
 
 	args := pd.Args{
